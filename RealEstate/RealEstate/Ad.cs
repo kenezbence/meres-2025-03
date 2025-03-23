d445fc5916a872424b92a7c1bf5fed1f02356b51
kenezbence <bencekenez1@gmail.com>
2025-03-23 22:02:02 +0100

    push

---
+++ RealEstate/RealEstate/Ad.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RealEstate
+{
+    public class Ad
+    {
+        public int Id { get; set; }          
+        public string Description { get; set; } 
+        public int Rooms { get; set; }        
+        public int Area { get; set; }         
+        public int Floors { get; set; }       
+        public Category Category { get; set; } 
+        public Seller Seller { get; set; }    
+        public bool FreeOfCharge { get; set; } 
+        public string ImageUrl { get; set; }  
+        public string LatLong { get; set; }   
+        public DateTime CreateAt { get; set; } 
+
+        
+        public double DistanceTo(double targetLat, double targetLon)
+        {
+            var parts = LatLong.Split(','); 
+            double lat = double.Parse(parts[0]); 
+            double lon = double.Parse(parts[1]); 
+            double dLat = lat - targetLat;       
+            double dLon = lon - targetLon;       
+            return Math.Sqrt(dLat * dLat + dLon * dLon); 
+        }
+
+       
+        public static List<Ad> LoadFromCsv(string fileName)
+        {
+            var ads = new List<Ad>(); 
+            using (var reader = new StreamReader(fileName))
+            {
+                reader.ReadLine(); 
+                while (!reader.EndOfStream) 
+                {
+                    var line = reader.ReadLine(); 
+                    var fields = line.Split(';');
+
+                    var ad = new Ad
+                    {
+                        Id = int.Parse(fields[0]),         
+                        Rooms = int.Parse(fields[1]),     
+                        LatLong = fields[2],              
+                        Floors = int.Parse(fields[3]),    
+                        Area = int.Parse(fields[4]),      
+                        Description = fields[5],           
+                        FreeOfCharge = fields[6] == "1",   
+                        ImageUrl = fields[7],              
+                        CreateAt = DateTime.Parse(fields[8]), 
+                        Seller = new Seller
+                        {
+                            Id = int.Parse(fields[9]),     
+                            Name = fields[10],             
+                            Phone = fields[11]            
+                        },
+                        Category = new Category
+                        {
+                            Id = int.Parse(fields[12]),    
+                            Name = fields[13]              
+                        }
+                    };
+                    ads.Add(ad); 
+                }
+            }
+            return ads; 
+        }
+    }
+}
---
+++ RealEstate/RealEstate/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RealEstate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            var ads = Ad.LoadFromCsv("realestates.csv");
+
+            // 1. feladat: Földszinti ingatlanok átlagos alapterülete
+            var groundFloorAds = ads.Where(ad => ad.Floors == 0).ToList();
+            if (groundFloorAds.Count > 0) // Ha van földszinti ingatlan
+            {
+                double averageArea = groundFloorAds.Average(ad => ad.Area);
+                Console.WriteLine($"Az eladásra kínált földszinti ingatlanok átlagos alapterülete: {averageArea:F2} m2");
+            }
+            else
+            {
+                Console.WriteLine("Nincs földszinti ingatlan.");
+            }
+
+            // 2. feladat: Legközelebbi tehermentes ingatlan az óvodához
+            double targetLat = 47.4164220114023;  
+            double targetLon = 19.066342425796986;
+            var freeAds = ads.Where(ad => ad.FreeOfCharge).ToList();
+            if (freeAds.Count > 0) 
+            {
+                var closestAd = freeAds.OrderBy(ad => ad.DistanceTo(targetLat, targetLon)).First();
+                double distance = closestAd.DistanceTo(targetLat, targetLon);
+                Console.WriteLine($"A legközelebbi tehermentes ingatlan id-je: {closestAd.Id}, távolsága: {distance:F2} fok");
+            }
+            else
+            {
+                Console.WriteLine("Nincs tehermentes ingatlan.");
+            }
+        }
+    }
+}
